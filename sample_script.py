f47bf243eb9528eb2c6f94fac21f2dc147cbe2d0
Eric Dill <edill@bnl.gov>
2014-11-20 16:47:01 -0500

    ENH: Improved sample script

--- sample_script.py
+++ sample_script.py
@@ -8,23 +8,42 @@ import epics
 import time 
 
 
-s_id = random.randint(0,1000)
-
-header_dict = compose_header(scan_id=s_id)
+created = False
+while not created:
+    try:
+        s_id = random.randint(0,1000)
+        header_dict = compose_header(scan_id=s_id)
+        created = True
+    except ValueError:
+        time.sleep(.5)
+        print('.')
 
 create_header(header_dict)
 
 
-print("sleeping for 3 seconds")
-time.sleep(3)
-desc_dict = compose_event_descriptor(header=header_dict,event_type_id=1, descriptor_name='scan')
-
-
-
+created = False
+while not created:
+    try:
+        desc_dict = compose_event_descriptor(header=header_dict,event_type_id=1, descriptor_name='scan')
+        created = True
+    except Exception:
+        # i strongly object to this exception, arman is a jackass
+        time.sleep(.5)
+        print('.')
 create_event_descriptor(desc_dict)
 
-event = compose_event(header_dict, desc_dict, 2)
+# print("sleeping for 3 seconds")
+# time.sleep(3)
+
+created = False
+while not created:
+    try:
+        event = compose_event(header_dict, desc_dict, 2)
+        created = True
+    except Exception:
+        # i strongly object to this exception, arman is a jackass
+        time.sleep(.5)
+        print('.')
+
 print('event: {}'.format(event))
-print("sleeping for 3 seconds")
-time.sleep(3)
 create_event(event)
