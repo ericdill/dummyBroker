0b11762089c84f7c34d0d6a7a482c7a5ef3b3bcc
arkilic <arkilic@bnl.gov>
2014-11-20 14:26:05 -0500

    First and only commit

---
+++ brokerServer.py
@@ -0,0 +1,47 @@
+import time
+import json
+import epics
+from metadataStore.collectionapi.commands import create, record
+
+
+header_PV = "XF:23ID-CT{Replay}Val:RunHdr-I"
+event_PV = "XF:23ID-CT{Replay}Val:EventHdr-I"
+
+
+
+def header_callback(value, **kw):
+    raw_data = kw['char_value']
+    data = json.loads(raw_data)
+    if data.has_key('header'):
+        create(header=data['header'])
+        print 'Created header entry'
+    if data.has_key('event_descriptor'):
+        create(event_descriptor=data['event_descriptor']) 
+        print 'Created event_descriptor entry'    
+
+def event_callback(value, **kw):
+    raw_data = kw['char_value']
+    data = json.loads(raw_data)
+    record(event=data)
+    print 'Created event entry'
+
+
+
+hdr_pv = epics.PV(header_PV, auto_monitor=True)
+hdr_pv.add_callback(header_callback)
+
+
+hdr_pv = epics.PV(event_PV, auto_monitor=True)
+hdr_pv.add_callback(event_callback)
+
+
+
+print 'Now wait for changes'
+
+t0 = time.time()
+#while time.time()-t0 < 300:
+while True:
+    time.sleep(0.01)
+
+
+print 'Broker stopped!'
---
+++ broker_commands.py
@@ -0,0 +1,164 @@
+__author__ = 'arkilic'
+import datetime
+from metadataStore.collectionapi.commands import find as _find
+from metadataStore.collectionapi.commands import create as _create
+from metadataStore.collectionapi.commands import record as _record
+import epics
+import json
+
+
+header_PV = "XF:23ID-CT{Replay}Val:EventHdr-I"
+event_PV = "XF:23ID-CT{Replay}Val:RunHdr-I"
+
+def compose_header(scan_id, **kwargs):
+    header = dict()
+    provided_keys = kwargs.keys()
+    for key in provided_keys:
+        if header_keys_dict.has_key(key):
+            if isinstance(kwargs[key], header_keys_dict[key]['type']):
+                pass
+            else:
+                raise TypeError('Given type is not valid for ', key, ' Should be ', header_keys_dict['type'])
+        else:
+            raise ValueError("The provided key is invalid for header", key)
+    header = kwargs
+    res = _find(scan_id = scan_id)
+    if res:
+        raise Exception('A scan with this id exists ', scan_id)
+    else:
+        header['scan_id'] = scan_id
+    return header
+
+
+def create_header(header):
+    header_pv = epics.PV(header_PV)
+    try:
+        #_create(header=header)
+        dumped_hdr = json.dumps(header)
+        print 'Writing to PV', dumped_hdr
+        header_pv.put(dumped_hdr)
+    except:
+        raise
+
+def create_event_descriptor(event_descriptor):
+    header_pv = epics.PV(header_PV)
+    try:
+        #_create(event_descriptor=event_descriptor)
+        dumped_ev_desc = json.dumps(event_descriptor)
+        header_pv.put(dumped_ev_desc)
+    except:
+        raise
+
+def create_event(event):
+    event_pv = epics.PV(event_PV)
+    try:
+        #_record(event=event)
+        dumped_ev = json.dumps(event)
+        event_pv.put(dumped_ev)
+    except:
+        raise
+
+def compose_event_descriptor(header, event_type_id, **kwargs):
+    event_descriptor = dict()
+    provided_keys = kwargs.keys()
+    for key in provided_keys:
+        if event_desc_keys_dict.has_key(key):
+            if isinstance(kwargs[key], event_desc_keys_dict[key]['type']):
+                pass
+            else:
+                raise TypeError('Given type is not valid for ', key, ' Should be ', header_keys_dict['type'])
+        else:
+            raise ValueError("The provided key is invalid for header", key)
+    event_descriptor = kwargs
+    scan_id = header['scan_id']
+    res = _find(scan_id = scan_id)
+    if res:
+        pass
+    else:
+        raise Exception('Header with given scan_id is not yet created')
+    event_descriptor["event_type_id"] = event_type_id
+    event_descriptor["scan_id"] = scan_id
+
+    return event_descriptor
+
+def compose_event(header, descriptor, seq_no, **kwargs):
+    event = dict()
+    provided_keys = kwargs.keys()
+    for key in provided_keys:
+        if event_keys_dict.has_key(key):
+            if isinstance(kwargs[key], event_keys_dict[key]['type']):
+                pass
+            else:
+                raise TypeError('Given type is not valid for ', key, ' Should be ', header_keys_dict['type'])
+        else:
+            raise ValueError("The provided key is invalid for header", key)
+    event = kwargs
+    event['seq_no'] = seq_no
+    scan_id = header['scan_id']
+    res = _find(scan_id = scan_id)
+    if res:
+        pass
+    else:
+        raise Exception('Header with given scan_id is not yet created')
+    
+    descriptor_name = descriptor["descriptor_name"]
+    event = kwargs
+    event["scan_id"] = scan_id
+    event["descriptor_name"] = descriptor_name
+    return event
+
+header_keys_dict = dict()
+#TODO: Change to OrderedDict
+
+header_keys_dict["scan_id"] = {"description": "The unique identifier of the run",
+                               "type": int}
+
+header_keys_dict["owner"] = { "description": "The user name of the person that created the header",
+                              "type": str}
+
+header_keys_dict["start_time"] = { "description": "The start time in utc",
+                                   "type": datetime.datetime}
+
+header_keys_dict["status"] = { "description": 'Run header completion status( In Progress/Complete',
+                               "type": str}
+
+header_keys_dict["beamline_id"] = { "description": "Beamline identifiying string",
+                                    "type": str}
+
+header_keys_dict["custom"] = { "description": " Additional attribute value fields that can be user defined",
+                               "type": dict}
+
+event_desc_keys_dict = dict()
+event_desc_keys_dict['header'] = { "description": " Run header associated with an event descriptor",
+                                   "type": dict}
+
+event_desc_keys_dict['event_type_id'] = { "description": " Run header associated with an event descriptor",
+                                          "type": int}
+
+event_desc_keys_dict['descriptor_name'] = { "description": "Name associated with given event type descriptor",
+                                            "type": str}
+
+event_desc_keys_dict['type_descriptor'] = { "description": " Additional information regarding event_type_desc",
+                                            "type": dict}
+
+
+event_keys_dict = dict()
+
+event_keys_dict['header'] = { "description": " Run header associated with an event descriptor",
+                                   "type": dict}
+
+event_keys_dict['event_descriptor'] = { "description": "Event descriptor asssociated with a specific event",
+                                             "type": dict}
+
+event_keys_dict['seq_no'] = { "description": "Sequence number for the event wrt other events",
+                                   "type": int}
+
+event_keys_dict['owner'] = { "description": "The user name of the person that created the header",
+                             "type": str}
+
+event_keys_dict['description'] = { "description": "String identifier describing nature of an event",
+                                   "type": str}
+
+event_keys_dict['data'] = { "description": "Data point name-value pair container",
+    "type": dict}
+
